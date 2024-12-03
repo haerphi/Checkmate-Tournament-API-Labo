34db79ef85f45657ad4652676c84fc87e1a8d034
haerphi <haerphi@gmail.com>
2024-12-03 11:58:49 +0100

    [Fix] Database StoredProcedure: AddPlayer

---
+++ Checkmate.Database/StoredProcedures/AddPlayer.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [Person].[AddPlayer]
+(
+    @pseudo NVARCHAR(50),
+    @email NVARCHAR(500),
+    @password NVARCHAR(1000),
+    @birthdate DATETIME2,
+    @gender CHAR(1),
+    @elo INT = 1200,
+    @role CHAR(1) = 'p',
+    @newPlayerId INT OUTPUT
+)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Insert the new player into the table
+    INSERT INTO [Person].[Player] (Pseudo, Email, Password, BirthDate, Gender, Elo, Role)
+    VALUES (@pseudo, @email, @password, @birthdate, @gender, @elo, @role);
+
+    -- Retrieve the ID of the newly inserted player
+    SET @newPlayerId = SCOPE_IDENTITY();
+END;
+GO
