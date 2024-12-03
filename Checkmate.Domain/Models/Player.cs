150ef7a799e6b8776f85e2c957cf019e4333045d
haerphi <haerphi@gmail.com>
2024-12-03 14:19:00 +0100

    [Fix] Domain model Player: optionnal id

--- Checkmate.Domain/Models/Player.cs
+++ Checkmate.Domain/Models/Player.cs
@@ -4,6 +4,7 @@ namespace Checkmate.Domain.Models
 {
 	public class Player
 	{
+		public int? Id { get; set; } = null;
 		public required string Nickname { get; set; }
 		public required string Email { get; set; }
 		public required string Password { get; set; }
