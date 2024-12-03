e87ea1a3ac8910473c2e565be571f491c6a7801c
haerphi <haerphi@gmail.com>
2024-12-03 11:51:14 +0100

    [Refacto+Add] Database: folders + add stored procedure CreatePlayerFn

---
+++ Checkmate.Database/StoredProcedures/CreatePlayerFn.sql
@@ -0,0 +1,19 @@
+﻿CREATE FUNCTION [Person].[CreatePlayerFn](
+	@pseudo NVARCHAR(50),
+	@email NVARCHAR(500),
+	@password NVARCHAR(1000),
+	@birthdate DATETIME2,
+	@gender CHAR(1),
+	@elo INT = 1200,
+	@role CHAR(1) = 'p')
+RETURNS INT
+AS 
+BEGIN
+	-- Insert the player and return its id
+	DECLARE @id INT
+	INSERT INTO [Person].[Player] ([Pseudo], [Email], [Password], [Birthdate], [gender], [elo], [role])
+		OUTPUT INSERTED.[Id] INTO @id
+		VALUES (@pseudo, @email, @password, @birthdate, @gender, @elo, @role)
+		
+	RETURN @id
+END
---
+++ Checkmate.Database/Tables/Player.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [Person].[Player]
+(
+	[Id] INT IDENTITY,
+    [Pseudo] NVARCHAR(50) NOT NULL, 
+    [Email] NVARCHAR(500) NOT NULL, 
+    [Password] NVARCHAR(1000) NOT NULL, 
+    [Birthdate] DATETIME2 NOT NULL, 
+    [Gender] CHAR(1) NULL, 
+    [ELO] INT NOT NULL DEFAULT 1200, 
+    [Role] CHAR(1) NOT NULL DEFAULT 'p',
+
+    CONSTRAINT [PK_Player_Id] PRIMARY KEY ([Id]),
+    CONSTRAINT [UQ_Player_Pseudo] UNIQUE ([Pseudo]),
+    CONSTRAINT [UQ_Player_Email] UNIQUE ([Email])
+)
