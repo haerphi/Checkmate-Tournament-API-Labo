2d2310b0934dff5de5bfaed57af8ae0b52d02e29
haerphi <haerphi@gmail.com>
2024-12-08 21:33:01 +0100

    [Fix] MM_Player_Tournament table: FK + delete cascade

--- Checkmate.Database/Tables/MM_Player_Tournament.sql
+++ Checkmate.Database/Tables/MM_Player_Tournament.sql
@@ -4,5 +4,7 @@
 	[TournamentId] INT NOT NULL,
 	[RegistrationDate] DATETIME2 NOT NULL DEFAULT GETDATE()
 
-	CONSTRAINT [PK_MM_Player_Tournament] PRIMARY KEY ([PlayerId], [TournamentId])
+	CONSTRAINT [PK_MM_Player_Tournament] PRIMARY KEY ([PlayerId], [TournamentId]),
+	CONSTRAINT [FK_MM_Player_Tournament_PlayerId] FOREIGN KEY ([PlayerId]) REFERENCES [Person].[Player] ([Id]),
+	CONSTRAINT [FK_MM_Player_Tournament_TournamentId] FOREIGN KEY ([TournamentId]) REFERENCES [Game].[Tournament] ([Id]) ON DELETE CASCADE
 )
